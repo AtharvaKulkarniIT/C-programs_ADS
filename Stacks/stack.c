4e59e15897b3deaf5c6bc3cd7ed806053bfd24e8
adnansadar <adnansadar11@gmail.com>
2019-08-29 15:06:18 +0530

    modified the code

--- Stacks/stack.c
+++ Stacks/stack.c
@@ -1,73 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int MAXSIZE = 8;       
 int stack[8];     
 int top = -1;            
 
-int isempty() {
-
+int isempty() 
+{
    if(top == -1)
       return 1;
    else
       return 0;
 }
    
-int isfull() {
-
+int isfull() 
+{
    if(top == MAXSIZE)
       return 1;
    else
       return 0;
 }
 
-int peek() {
-   return stack[top];
+int peek()
+{
+    printf("\nElement on the top: %d \n",stack[top]);
 }
 
-int pop() {
-   int data;
-	
-   if(!isempty()) {
+int pop()
+ {
+   int data;	
+   if(!isempty()) 
+   {
       data = stack[top];
       top = top - 1;   
-      return data;
+      printf("\nElement deleted %d\n",data );
    }
-   else {
-      printf("Could not retrieve data, Stack is empty.\n");
+   else 
+   {
+      printf("\nCould not retrieve data, Stack is empty.\n");
    }
 }
 
-int push(int data) {
-
+int push()
+{
+   int data;
+   printf("Enter element: ");
+   scanf("%d",&data);
    if(!isfull()) {
       top = top + 1;   
       stack[top] = data;
    } 
-   else {
-      printf("Could not insert data, Stack is full.\n");
+   else 
+   {
+      printf("\nCould not insert data, Stack is full.\n");
    }
 }
 
-int main() {
-   // push items on to the stack 
-   push(3);
-   push(5);
-   push(9);
-   push(1);
-   push(12);
-   push(15);
-
-   printf("Element at top of the stack: %d\n" ,peek());
-   printf("Elements: \n");
-
-   // print stack data 
-   while(!isempty()) {
-      int data = pop();
-      printf("%d\n",data);
+int main() 
+{
+   int key;
+   while(1)
+   {
+      printf("\n1. Push element in the stack \n");
+      printf("2. Pop element from the stack \n");
+      printf("3. Peek \n");
+      printf("4. Exit \n");
+      printf("Enter your choice: \n");
+      scanf("%d",&key);
+   switch(key)
+   {
+      case 1:
+         push();
+         break;
+      case 2:
+         pop();
+         break;
+      case 3:
+         peek();
+         break;
+      case 4:
+      exit(1);
+      default:
+      printf("Wrong input!\n\n");
    }
-
-   printf("Stack full: %s\n" , isfull()?"true":"false");
-   printf("Stack empty: %s\n" , isempty()?"true":"false");
+   }
    
    return 0;
 }
